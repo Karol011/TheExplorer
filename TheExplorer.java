16cf1c652d1f49ca5e6ec8c643a20984ad0997b9
Karol011 <karollacek1@gmail.com>
2023-08-28 10:47:00 +0200

    setting up main window and background color

--- TheExplorer.java
+++ TheExplorer.java
@@ -1,5 +1,18 @@
+import javax.swing.*;
+
 public class TheExplorer {
     public static void main(String[] args) {
 
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("TheExplorer");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
     }
 }
